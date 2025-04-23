2118aeec70893194a6dae140faba6d401df20125
Oaken09 <romankoshkin2011@gmail.com>
2025-04-23 22:01:00 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,73 +4,47 @@ from tkn import my_token
 
 bot = telebot.TeleBot(my_token)
 
-keyboard = types.InlineKeyboardMarkup()
-button1 = types.InlineKeyboardButton(text='Мой курс', callback_data='course')
-keyboard.add(button1)
-button2 = types.InlineKeyboardButton(text='Из чего состоит курс?', callback_data='aboutcourse')
-keyboard.add(button2)
-button3 = types.InlineKeyboardButton(text='Дополнительные советы', callback_data='other')
-keyboard.add(button3)
-button4 = types.InlineKeyboardButton(text='Источники', callback_data='sources')
-keyboard.add(button4)
+markup_inline = types.InlineKeyboardMarkup()
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "/start":
-        bot.send_message(message.from_user.id, "Привет, очень хорошо, что ты осознал проблему и хочешь её решить. \nВыбери один из пунктов ниже:", reply_markup=keyboard)
+b_course = types.InlineKeyboardButton("Мой курс", callback_data="Мой курс")
+b_aboutcourse = types.InlineKeyboardButton("Из чего состоит курс?", callback_data="Из чего состоит курс?")
+b_other = types.InlineKeyboardButton("Дополнительные советы", callback_data="Дополнительные советы")
+b_sources = types.InlineKeyboardButton("Источники", callback_data="Источники")
+b_quiz = types.InlineKeyboardButton("Викторина", callback_data="Викторина")
 
-    else:
-        bot.send_message(message.from_user.id, "Напиши /start")
+markup_inline.add(b_course)
+markup_inline.add(b_other, b_sources)
+markup_inline.add(b_quiz, b_aboutcourse)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Привет! Выбери кнопку:", reply_markup=markup_inline)
+
 
 @bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    if call.data == "course":
-        msg = "Выбери от чего ты хочешь избавиться:"
-        button5 = types.InlineKeyboardButton(text='Сигареты', callback_data='smoking')
-        keyboard.add(button5)
-        button6 = types.InlineKeyboardButton(text='Вейп', callback_data='vaping')
-        keyboard.add(button6)
-        button7 = types.InlineKeyboardButton(text='Алкоголь', callback_data='alcohol')
-        keyboard.add(button7)
-        button8 = types.InlineKeyboardButton(text='"Энергетики', callback_data='enrgy')
-        keyboard.add(button8)
-        button9 = types.InlineKeyboardButton(text='На главную', callback_data='first')
-        keyboard.add(button9)
+def handle_callback_query(call):
+    received_data = call.data
 
-        keyboard = types.InlineKeyboardMarkup()
-        key_PrKv_Pr = types.InlineKeyboardButton(text='ПРЯМОУГОЛЬНИК', callback_data='Rectangle')
-        keyboard.add(key_PrKv_Pr)
-        key_PrKv_Kv = types.InlineKeyboardButton(text='КВАДРАТ', callback_data='Square')
-        keyboard.add(key_PrKv_Kv)
+    bot.delete_message(call.message.chat.id, call.message.message_id)
 
-        bot.send_message(call.message.chat.id, msg)
-        bot.send_message(call.message.chat.id, "Выбери нужную фигуру:", reply_markup=keyboard)
+    bot.answer_callback_query(call.id)
 
-    elif call.data == "Rectangle":
-        bot.send_message(call.message.chat.id, "Это информация о прямоугольнике...")
-    elif call.data == "Square":
-        bot.send_message(call.message.chat.id, "Это информация о квадрате...")
-    elif call.data == "triangle":
-        msg = "Сейчас я ОЛЕГ покажу, что я знаю о треугольниках."
-
-        keyboard = types.InlineKeyboardMarkup()
-        key_Tri_Pr = types.InlineKeyboardButton(text='ПРЯМОУГОЛЬНЫЙ ТРЕУГОЛЬНИК', callback_data='triangle PR')
-        keyboard.add(key_Tri_Pr)
-        key_Tri_Or = types.InlineKeyboardButton(text='ОСТРОУГОЛЬНЫЙ ТРЕУГОЛЬНИК', callback_data='triangle OS')
-        keyboard.add(key_Tri_Or)
-        key_Tri_Ty = types.InlineKeyboardButton(text='ТУПОУГОЛЬНЫЙ ТРЕУГОЛЬНИК', callback_data='triangle TY')
-        keyboard.add(key_Tri_Ty)
-
-        bot.send_message(call.message.chat.id, msg)
-        bot.send_message(call.message.chat.id, "Выбери тип треугольника:", reply_markup=keyboard)
-
-    elif call.data == "triangle PR":
-        bot.send_message(call.message.chat.id, "Это информация о прямоугольном треугольнике...")
-    elif call.data == "triangle OS":
-        bot.send_message(call.message.chat.id, "Это информация об остроугольном треугольнике...")
-    elif call.data == "triangle TY":
-        bot.send_message(call.message.chat.id, "Это информация о тупоугольном треугольнике...")
+    if received_data == "Мой курс":
+        bot.send_message(call.message.chat.id, "Привет, очень хорошо, что ты осознал проблему и хочешь её решить. \nВыбери один из пунктов ниже:")
+    elif received_data == "Из чего состоит курс?":
+        bot.send_message(call.message.chat.id, "Текст для 'Из чего состоит курс?'")
+    elif received_data == "Дополнительные советы":
+        bot.send_message(call.message.chat.id, "Текст для 'Дополнительные советы'")
+    elif received_data == "Источники":
+        bot.send_message(call.message.chat.id, "Текст для 'Источники'")
+    elif received_data == "Викторина":
+         bot.send_message(call.message.chat.id, "Текст для 'Викторина'")
 
 
-# Запускаем бота
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.send_message(message.chat.id, f"Я получил твое сообщение: '{message.text}'. \nНажимай на кнопки под предыдущим сообщением, если хочешь выбрать пункт.")
+
+
 bot.polling(none_stop=True)
